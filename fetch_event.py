b850f04dcf369b23384af6a2e3d9176c09eed38b
tonisbones <decausem@gmail.com>
2017-10-24 16:34:39 -0400

    made fetch_event use a list on disk

--- fetch_event.py
+++ fetch_event.py
@@ -13,14 +13,25 @@ def get_facebook_graph():
 def get_event_info(graph, event_id):
     return graph.get_object(event_id) 
 
+def get_event_list(event_list_disk):
+    event_list = {}
+    with open(event_list_disk, 'r') as event_list_file:
+        event_list = event_list_file.read().splitlines()
+    event_list_file.close()
+    return event_list
+
 if __name__ == "__main__":
+    event_list_disk = "event_list.txt"
     graph = get_facebook_graph()
     print("Got graph")
-    event_id = sys.argv[1]
-    fb_event = get_event_info(graph, event_id)
-    print(fb_event)
-    new_file_name = '../facebookeventjsondump/events/' + event_id + '.json'
-    with open(new_file_name, 'w') as event_json_file:
-        json.dump(fb_event, event_json_file)
+    event_list = get_event_list(event_list_disk); 
+    for event_id in event_list:
+        if (event_id is not None and event_id != ""):
+            print(event_id)
+            fb_event = get_event_info(graph, event_id)
+            print(fb_event)
+            new_file_name = '../facebookeventjsondump/events/' + event_id + '.json'
+            with open(new_file_name, 'w') as event_json_file:
+                json.dump(fb_event, event_json_file)
 
-    event_json_file.close()
+            event_json_file.close()
