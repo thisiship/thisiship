3a55040e840403a436d36bd35f5bd20965a41f05
tonisbones <decausem@gmail.com>
2017-11-21 14:07:54 -0500

    fixed index bug

--- create_index.py
+++ create_index.py
@@ -124,7 +124,7 @@ def create_event_block(ev_data):
                             <span class="glyphicon glyphicon-info-sign"></span>
                             View Details
                         </button>
-                        <a href="http://www.facebook.com/events/{ev_id}" class="btn fb-link" id="fb-{ev_id} role="button" target="_blank" data-toggle="tooltip" title="View Event On Facebook"><i class="fa fa-facebook"></i></a>"""
+                        <a href="http://www.facebook.com/events/{ev_id}" class="btn fb-link" id="fb-{ev_id}" role="button" target="_blank" data-toggle="tooltip" title="View Event On Facebook"><i class="fa fa-facebook"></i></a>"""
     modal_html ="""
                         <div class="modal fade" id="modal-{ev_id}" tabindex="-1" role="dialog" aria-labelledby="EventDetails">
                             <div class="modal-dialog" role="document">
