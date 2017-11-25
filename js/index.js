b118645426a136914bc89756d926facf5f4f6ecd
tonisbones <decausem@gmail.com>
2017-11-25 15:34:16 -0500

    GA round 12 :)

--- js/index.js
+++ js/index.js
@@ -63,7 +63,8 @@ $(document).ready(function() {
 	$(".desc-btn").click(function() {
 		var event_id = $(this).siblings(".ev-id").first().text().trim();
 		console.log("Sending info to GA: "  + event_id);
-		gtag('event', 'description', event_id, {
+		gtag('event', 'description', {
+			'value': event_id,
 			hitCallback: function() {
 				console.log('Description for ' + event_id + ' sent to analytics.');
 			}
