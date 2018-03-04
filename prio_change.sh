d7a5a1507e6eb2dc1a24d8bf0dddbdd2e5fc5978
tonisbones <decausem@gmail.com>
2018-03-03 21:09:41 -0500

    added srcipt to change prio to 8 for personal promoting

---
+++ prio_change.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSONAL_PRIO="8"
+
+echo "Personal Promotion. Set to Priority $PERSONAL_PRIO:"
+for promoted in "elephino" "the english project" "the forest dwellers"
+do
+	echo $promoted
+	#																	need to use variable here
+	grep -il "$promoted" jsondump/* | xargs sed -i -e 's/"priority": "[0-9]"/"priority": "8"/g'
+done
+
