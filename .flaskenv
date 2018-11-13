c3ef2afb81f7ea4f28c18855322c345250f40a00
tonisbones <decausem@gmail.com>
2018-11-13 14:06:42 -0500

    chapter 10

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,3 @@
 FLASK_APP=thisiship.py
+MAIL_SERVER=localhost
+MAIL_PORT=8025
--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -11,6 +12,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
